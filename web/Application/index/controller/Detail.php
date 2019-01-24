deb5764969ebafc55a8f43803374dc6ebb1081ce
BugAngel <1055056138@qq.com>
2019-01-24 14:31:25 +0800

    完成行前准备、学习指南、课程对照、公共用语四部分开发

---
+++ web/Application/index/controller/Detail.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/1/24/024
+ * Time: 10:58
+ */
+
+namespace app\index\controller;
+
+use think\Controller;
+
+class Detail extends Controller
+{
+    public function xingqian()
+    {
+        $this->assign('title',"行前准备");
+        return $this->fetch();
+    }
+
+    public function xuexi()
+    {
+        $this->assign('title',"学习指南");
+        return $this->fetch();
+    }
+
+    public function kecheng()
+    {
+        $this->assign('title',"课程对照");
+        return $this->fetch();
+    }
+
+    public function gonggong()
+    {
+        $this->assign('title',"公共用语");
+        return $this->fetch();
+    }
+}
