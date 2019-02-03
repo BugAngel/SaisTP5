e19e83f130325daab3e975201692d19f1d0cc03f
BugAngel <1055056138@qq.com>
2019-02-03 18:17:21 +0800

    使用bootstarp修改footer并解决兼容性问题，使用bootstarp的表单与select完成院校选择页面的提交

--- web/Application/index/controller/School.php
+++ web/Application/index/controller/School.php
@@ -14,19 +14,74 @@ use think\Controller;
 
 class School extends Controller
 {
-    public function index($area=0)
+    public function test()
     {
         $this->assign('title',"院校自选");
+
+        trace("进入后台");
+        $country="美国";
+        trace($country);
+        $qsLow="1";
+        $qsHigh="10";
+        $majorLow="1";
+        $majorHigh="10";
+
         $map=[];
-        switch ($area) {
-            case 1:
-                $map['country'] = array('in','美国');
-                break;
-            case 2:
-                $map['country'] = array('in','英国');
-                break;
+
+        $map[0] = ['world_rank','between',[$qsLow, $qsHigh]];
+        $map[1] = ['major_rank','between',[$majorLow, $majorHigh]];
+        if($country!="全部")
+        {
+            $map[2]=['country','=',$country];
         }
+        trace($map);
+        $list = CollegeInfo::all(function($query)use($map,$qsLow, $qsHigh){
+//            trace($map);
+            $query->where('world_rank','between',[$qsLow, $qsHigh])->order('world_rank', 'asc');
+        });
+        $college=[];
+        foreach ($list as $school) {
+            $college[]=[
+                'href'=>$school->college_name,
+                'icon'=>$school->icon,
+                'college_name'=>$school->college_name,
+                'college_e_name'=>$school->college_e_name,
+                'major_rank_name'=> $school->major_rank_name,
+                'major_rank'=> $school->major_rank,
+                'world_rank'=> $school->world_rank,
+                'rate'=> $school->rate,
+                'country'=> $school->country,
+                'area'=> $school->area,
+                'hot_major'=> implode("、",$school->hot_major),
+            ];
+        }
+        trace($college);
+        return $this->fetch('index',['college'=>$college]);
+    }
+
+    public function index()
+    {
+        $this->assign('title',"院校自选");
+
+        trace("进入后台");
+        $country=input('country');
+        trace("country的值",$country);
+        $qsLow=input('qsLow');
+        $qsHigh=input('qsHigh');
+        $majorLow=input('majorLow');
+        $majorHigh=input('majorHigh');
+
+        $map=[];
+
+        $map[0] = ['world_rank','between',[$qsLow, $qsHigh]];
+        $map[1] = ['major_rank','between',[$majorLow, $majorHigh]];
+        if($country!="全部")
+        {
+            $map[2]=['country','=',$country];
+        }
+
         $list = CollegeInfo::all(function($query)use($map){
+//            trace($map);
             $query->where($map)->order('world_rank', 'asc');
         });
         $college=[];
